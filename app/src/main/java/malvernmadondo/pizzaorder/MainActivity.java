a6f8e4aee2e064828fa533882a3feeff390c100b
mmadondo <madondomalvern@gmail.com>
2017-02-05 14:36:06 -0600

    Added comments

--- app/src/main/java/malvernmadondo/pizzaorder/MainActivity.java
+++ app/src/main/java/malvernmadondo/pizzaorder/MainActivity.java
@@ -28,21 +28,21 @@ public class MainActivity extends AppCompatActivity {
       Double totalPrice = 0.00;
 
         if(rbSmall.isChecked()){
-            totalPrice = totalPrice + 7.00;
+            totalPrice = totalPrice + 7.00; //for small pizza
         }
         else if(rbMedium.isChecked()){
-            totalPrice = totalPrice + 9.00;
+            totalPrice = totalPrice + 9.00; //for medium-sized pizza
         }
         else if(rbLarge.isChecked()){
-            totalPrice = totalPrice + 13.00;
+            totalPrice = totalPrice + 13.00; //for large pizza
         }
 
         //Extras
         if(cbCheese.isChecked()){
-            totalPrice = totalPrice + 1.75;
+            totalPrice = totalPrice + 1.75; //for additional cheese
         }
         if(cbDelivery.isChecked()){
-            totalPrice = totalPrice + 2.50;
+            totalPrice = totalPrice + 2.50; //delivery
         }
        tvTotal.setText(String.format("Total is $%.2f", totalPrice));
     }
